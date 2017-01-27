4158af7f7a4563f57a3fbf83ac3b888f6ebcfbbb
thenaz <ammarznazir@gmail.com>
2017-01-27 08:05:08 -0700

    Initial Commit

---
+++ SecondC++Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SecondC++Project
+//
+//  Created by Nazir, Ammar on 1/27/17.
+//  Copyright © 2017 Nazir, Ammar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
