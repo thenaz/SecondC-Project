692b12dec3517bb29be83fd606aeb9b2700a79a6
thenaz <ammarznazir@gmail.com>
2017-01-31 09:29:33 -0700

    Added framework to Timer.cpp

--- SecondC++Project/Timer.cpp
+++ SecondC++Project/Timer.cpp
@@ -7,3 +7,37 @@
 //
 
 #include "Timer.hpp"
+#include <iostream>
+
+using namespace std;
+
+Timer :: Timer()
+{
+    executionTime = 0;
+}
+
+void Timer :: resetTimer()
+{
+    executionTime = 0;
+}
+
+void Timer :: startTimer()
+{
+    executionTime = clock();
+}
+
+void Timer :: stopTimer()
+{
+    executionTime = clock() - executionTime;
+}
+
+void Timer :: displayTimerInformation()
+{
+    cout << "The execution time is: " <<  executionTime << endl;
+    cout << "In human time it is " << executonTime/CLOCKS_PER_SEC << " seconds" << endl;
+}
+
+long Timer :: getExecutionTimeInMicroseconds()
+{
+    return executionTime;
+}
